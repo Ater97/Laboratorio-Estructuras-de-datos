c56a535da44584d7212b804225e118195b07ba36
Bryan Macario Coronado <bryan.macario12@gmail.com>
2017-03-20 07:38:36 -0600

    It's was add changes visual

--- LABORATORIO 2/Laboratory_2/Laboratory_2/App_Start/BundleConfig.cs
+++ LABORATORIO 2/Laboratory_2/Laboratory_2/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace Laboratory_2
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/sitePersonalized.css"));
         }
     }
 }
