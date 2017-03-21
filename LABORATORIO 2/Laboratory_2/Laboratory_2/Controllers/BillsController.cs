86fd4f8a8fb742e399e5384a7437cfabcaa6b61c
sebastian orantes <sebastian.orantes.chang@gmail.com>
2017-03-21 16:58:57 -0600

    Search bills
    doesn't work

--- LABORATORIO 2/Laboratory_2/Laboratory_2/Controllers/BillsController.cs
+++ LABORATORIO 2/Laboratory_2/Laboratory_2/Controllers/BillsController.cs
@@ -21,7 +21,22 @@ namespace Laboratory_2.Controllers
 
             return View(mostrar);
         }
+        [HttpPost]
+        public ActionResult Search(string serie, string correlative)
+        {
+           BillsModel temp = SearchElement(serie + "|" + correlative);
 
+            if (temp == null)
+            {
+                ViewBag.Message = "Cantidad de elementos: " + Singleton.Instance.BillsBinaryTree.GetCount();
+                ViewBag.Error = "Error";
+                ViewBag.Error1 = "El elemento buscado no se ha encontrado en la base de datos.";
+                ViewBag.Error2 = "Vuelva a intentarlo de nuevo.";
+                return View("Index", Singleton.Instance.BillsBinaryTree);
+            }
+
+            return View("Details", temp);
+        }
         public ActionResult UploadBillDescription()
         {
             return View();
